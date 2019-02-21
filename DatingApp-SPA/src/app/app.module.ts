b65fdf2b7ff0ca4e3df1b72fc1f23b30920ceb68
andywilliamson-viewpointcs <andy.williamson@viewpoint.com>
2019-02-21 13:09:43 -0800

    End of section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './Nav/Nav.component';
-import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './Home/Home.component';
-import { RegisterComponent } from './Register/Register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./Nav/Nav.component";
+import { AuthService } from "./_services/auth.service";
+import { HomeComponent } from "./Home/Home.component";
+import { RegisterComponent } from "./Register/Register.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { AlertifyService } from "./_services/alertify.service";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [AuthService, ErrorInterceptorProvider],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot()],
+  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
